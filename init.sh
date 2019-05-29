ac776320afe9731874c8ba8d43e9f48eea323002
Gary Teh <changsiongteh@gmail.com>
2019-05-29 22:50:39 +1000

    Update as Mojave does not suppose -v anymore

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ if [[ ! -e "/Library/Developer/CommandLineTools/usr/bin/git" ]]; then
     echo "Installing Xcode Command Line Tool..."
     touch "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
     PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
-    softwareupdate -i "$PROD" -v
+    softwareupdate -i "$PROD" --verbose
 fi
 
 if ! which brew &> /dev/null; then
