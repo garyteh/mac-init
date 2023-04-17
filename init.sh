8836ab3072858eff5bc8dbac9b8a15c20b9dfb01
Gary Teh <gteh@atlassian.com>
2023-04-17 22:21:11 +1000

    Fix NONINTERACTIVE as it fail complaining it needs sudo password

--- init.sh
+++ init.sh
@@ -27,7 +27,7 @@ install_homebrew() {
 
     if ! which brew &> /dev/null; then
         echo "Installing Homebrew..."
-        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &> "${LOG_FILE}"
+        echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &> "${LOG_FILE}"
     fi
     if [[ "${UNAME_MACHINE}" == "arm64" ]]; then
         # On ARM macOS, this script installs to /opt/homebrew only
